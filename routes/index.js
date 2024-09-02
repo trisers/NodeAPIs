7c9037d13bf6b991f080caf6fb810f0905bb6794
trisers <trisers11@gmail.com>
2024-09-02 16:06:00 +0530

    Add(order api): Added order CRUD apis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import productRoutes from "../routes/product.js";
+import orderRoutes from "../routes/order.js";
 const routes = express.Router();
 
 // Use imported routes
 routes.use("/product", productRoutes);
+routes.use("/order", orderRoutes);
 
 // Add more routes here as needed
 // router.use('/anotherRoute', anotherRouteFile);
