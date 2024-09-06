ee42e8f26be41a10e13bbbc3b7e943515ae8d9ea
trisers <trisers11@gmail.com>
2024-09-06 17:53:41 +0530

    feat(dashboard-user):Added dashboard user feature

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import orderRoutes from "./order.js";
 import authRoutes from "./authentication.js";
 import userRoutes from "./user.js";
 import capabilitiesRoutes from "./capabilities.js";
+import dashboardUserRoutes from "./dashboard-user.js";
 import { verifySuperAdminToken } from "../middlewares/superadmin.js";
 const routes = express.Router();
 
@@ -13,6 +14,7 @@ routes.use("/order", orderRoutes);
 routes.use("/auth", authRoutes);
 routes.use("/user", userRoutes);
 routes.use("/capabilities", verifySuperAdminToken, capabilitiesRoutes);
+routes.use("/dashboard-user", verifySuperAdminToken, dashboardUserRoutes);
 
 // Add more routes here as needed
 // router.use('/anotherRoute', anotherRouteFile);
